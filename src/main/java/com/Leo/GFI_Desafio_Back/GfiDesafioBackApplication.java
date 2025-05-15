ecf4dcbd77a81e435c8be8295845b7db0cbe837d
Leonardo Sousa <38815972+LeoRSousa@users.noreply.github.com>
2025-05-15 09:47:10 -0300

    Init Commit

---
+++ src/main/java/com/Leo/GFI_Desafio_Back/GfiDesafioBackApplication.java
@@ -0,0 +1,13 @@
+package com.Leo.GFI_Desafio_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfiDesafioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfiDesafioBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Leo/GFI_Desafio_Back/GfiDesafioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Leo.GFI_Desafio_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfiDesafioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
